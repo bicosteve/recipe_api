be40dad5c149054115d8af1864e6b2450211e228
Bico Steve <bicosteve4@gmail.com>
2020-03-26 09:43:07 +0300

    Added create recipe feature

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -65,3 +65,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return serializers.RecipeDetailSerializer
 
         return self.serializer_class
+
+    def perform_create(self,serializer):
+        '''create a new recipe'''
+
+        serializer.save(user=self.request.user)
