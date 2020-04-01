fa12c8e56ec622b89f0742d5ea09ab8e4fe22fc7
Bico Steve <bicosteve4@gmail.com>
2020-04-01 08:16:42 +0300

    Implimented filtering for tags and ingredients

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -41,8 +41,16 @@ class IngredientViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         '''returns objects  for the current authenticated user only'''
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only',0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
 
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        return queryset.filter(
+            user=self.request.user
+            ).order_by('-name').distinct()
 
     def perform_create(self,serializer):
         '''create a new ingredient'''
