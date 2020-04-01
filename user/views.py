8f3bd3a0710686e5a67b42226bbce87168dd2a12
Bico Steve <bicosteve4@gmail.com>
2020-04-01 07:12:06 +0300

    Added image upload feature

--- user/views.py
+++ user/views.py
@@ -1,9 +1,11 @@
+
 from rest_framework import generics,authentication,permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
 
 from user.serializers import UserSerializer,AuthTokenSerializer
+
 
 class CreateUserView(generics.CreateAPIView):
     '''create a new user in the system'''
